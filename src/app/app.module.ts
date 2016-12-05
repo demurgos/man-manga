9f72bd13ca7395752a051f031aa4f0d202c33580
Sn0wFox <ruben.pericas@laposte.net>
2016-12-05 11:26:02 +0100

    Added module for the /app

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+
+import {HomeModule} from './home/home.module';
+import {AboutModule} from './about/about.module';
+import {SearchModule} from './search/search.module';
+
+@NgModule({
+	imports: [
+		HomeModule,
+		AboutModule,
+		SearchModule
+	],
+})
+export class AppModule {
+}
