d98b45adc6002dc4f6b360de1bbcaf49b8a46b51
HugoMoy <hugo.moynac@gmail.com>
2016-12-07 10:58:13 +0100

    add response components, not yet implemented and styled

---
+++ src/app/Response/response.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+
+import {AnimeComponent} from './anime-response/anime-response.component';
+import {AuthorComponent} from './author-response/author-response.component';
+import {MangaComponent} from './manga-response/manga-response.component';
+import {ResponseRoutingModule} from "./response-routing.module";
+
+@NgModule({
+  imports: [
+    ResponseRoutingModule
+  ],
+  declarations: [
+    AnimeComponent,
+    AuthorComponent,
+    MangaComponent
+  ],
+  exports: [
+    AnimeComponent,
+    AuthorComponent,
+    MangaComponent
+  ]
+})
+export class ResponseModule {}
