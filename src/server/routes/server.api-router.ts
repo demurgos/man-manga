af458122f11f9c3e745340223081b58c9157a18d
Sn0wFox <ruben.pericas@laposte.net>
2016-12-20 15:18:47 +0100

    Added pipeline from google query to spotlighting

--- src/server/routes/server.api-router.ts
+++ src/server/routes/server.api-router.ts
@@ -4,9 +4,11 @@ import {
   IRouter}                from "express-serve-static-core";
 
 import {anilistApiRouter} from './server.api.anilist';
-import {apiRouter}  from './server.api.manmanga';
+import {apiRouter}        from './server.api.manmanga';
 
-import * as Google from '../lib/googlesearch';
+import * as Google        from '../lib/googlesearch';
+import * as Alchemy       from '../lib/alchemy';
+import * as Spotlight     from '../lib/spotlight';
 
 const router: Router = Router();
 
@@ -25,11 +27,23 @@ router.get("/api/test", (req: any, res: any) => {
  */
 router.get("/api/pipeline/:query", (req: any, res: any) => {
   let query: string = req.params["query"];
+  res.setHeader('Content-Type', 'application/json');
+  console.log("QUERYING...");
   Google
     .query(query)
     .then((result: any) => {
       console.log(result);
-      res.status(200).send(JSON.stringify(result, null, 2));
+      console.log("ALCHEMYING...");
+      return Alchemy.getTextFromURL(result[0]);
+    })
+    .then((result: Alchemy.Result) => {
+      console.log(result);
+      console.log("SPOTLIGHTING...");
+      return Spotlight.query(result.text, result.language);
+    })
+    .then((result: string[]) => {
+      console.log(result);
+      res.status(200).send(result);
     })
     .catch((err: any) => {
       res.status(500).send(err);
