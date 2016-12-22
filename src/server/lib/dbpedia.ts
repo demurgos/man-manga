a85fa784fe3761b84ec6fec5ac2a1b1053a165e2
Sn0wFox <ruben.pericas@laposte.net>
2016-12-22 11:46:56 +0100

    Completed DBPedia lib

--- src/server/lib/dbpedia.ts
+++ src/server/lib/dbpedia.ts
@@ -2,18 +2,13 @@ import * as Bluebird  from 'bluebird';
 import * as request   from 'request-promise';
 import * as _         from 'lodash';
 
-import {Manga}      from "../../lib/interfaces/manga.interface";
-import {Anime}      from "../../lib/interfaces/anime.interface";
-import {Character}  from "../../lib/interfaces/character.interface";
-import {Author}     from "../../lib/interfaces/author.interface";
+import {Manga as MangaType}         from "../../lib/interfaces/manga.interface";
+import {Anime as AnimeType}         from "../../lib/interfaces/anime.interface";
+import {Character as CharacterType} from "../../lib/interfaces/character.interface";
+import {Author as AuthorType}       from "../../lib/interfaces/author.interface";
 
-/**
- * Redeclare types to avoid conflict with the following namespace.
- */
-type MangaType = Manga;
-type AnimeType = Anime;
-type CharacterType = Character;
-type AuthorType = Author;
+// TODO: typings need to be remastered for this file - beware, it's kinda hard
+// Potential fix: use interfaces RawManga, RawAnime...
 
 /**
  * This namespace contains all functions needed by our app.
@@ -43,10 +38,25 @@ export namespace DBPedia {
      * A result from a dbpedia search request.
      */
     export interface Result {
-      manga?: Manga;
-      anime?: Anime;
-      author?: Author;
-      character?: Character;
+      manga?: MangaType;
+      anime?: AnimeType;
+      author?: AuthorType;
+      character?: CharacterType;
+    }
+
+    /**
+     * Result from a DBPedia sparql query,
+     * in a raw shape.
+     */
+    export interface RawResult {
+      x?: string;               // "manga", "anime" and so on
+      title?: string;           // Anime/Manga's title or people's name
+      author?: string;          // Author of the manga/anime
+      volumes?: number;         // Number of volumes or episodes
+      publicationDate?: string; // First volume/episode publication date or people's birthday
+      illustrator?: string;     // Illustrator of the manga/anime
+      publisher?: string;       // Publisher's name
+      abstract?: string;        // Manga/Anime's abstract or snippet about people
     }
 
     /**
@@ -70,7 +80,7 @@ export namespace DBPedia {
           + "OPTIONAL { ?title dbo:abstract ?abstract. filter(langMatches(lang(?abstract),'" + lang +"')) }."
         + "} UNION {"
           + "values ?title {<" + Utils.stringToResourceUrl(name) + ">}. "
-          + "bind(exists { ?title a dbo:Author. } as ?is)." // TODO: correct this
+          + "bind(exists { ?m a dbo:Manga. ?m dbo:author ?title. } as ?is)."
           + "bind(IF(?is=1, 'author', 0) as ?x)."
         + "} filter(?is != 0). } ";
       return request({
@@ -79,7 +89,21 @@ export namespace DBPedia {
       })
       .then((body: any) => {
         console.log(body["results"]["bindings"]);
-        return sparqlToObjects(body["results"]["bindings"]);
+        let res = DBPediaTransform.sparqlToObjects(body["results"]["bindings"]);
+        const mres = res.manga;
+        // NB: need a const variable (!= immutable) to be sure that condition will still be OK after promise - typescript
+        if(mres && mres.author) {
+          let authorName: any = mres.author;
+          return Author
+            .retrieve(authorName)
+            .then((author: AuthorType) => {
+              mres.author = author;
+              res.manga = mres;
+              return res;
+            });
+        }
+        // TODO: do the same for other potential results
+        return res;
       })
       .catch((err: any) => {
         return Bluebird.reject(err);
@@ -92,11 +116,12 @@ export namespace DBPedia {
    */
   export namespace Manga {
 
+    import RawResultArray = DBPediaTransform.RawResultArray;
     /**
      * Returns all available information about the manga 'name'.
      * @param name The manga's name.
      */
-    export function retrieve(name: string): Bluebird<Manga> {
+    export function retrieve(name: string): Bluebird<MangaType> {
       let manga: MangaType;
       return getInfos(name)
         .then((infos: any) => {
@@ -119,8 +144,7 @@ export namespace DBPedia {
      * @param mangaName The manga's name.
      * @param lang The lang in which the abstract is wanted. Default to english.
      */
-    // TODO: type this
-    export function getInfos(mangaName: string, lang: string = 'en'): Bluebird<any> {
+    export function getInfos(mangaName: string, lang: string = 'en'): Bluebird<MangaType> {
       console.log(Utils.stringToResourceUrl(mangaName));
       let query: string = "select distinct ?title ?author ?volumes ?publicationDate ?illustrator ?publisher ?abstract "
         + "where {"
@@ -138,7 +162,7 @@ export namespace DBPedia {
         json: true
       })
       .then((body: any) => {
-        return sparqlToManga(crossArray(body["results"]["bindings"]));
+        return DBPediaTransform.sparqlToManga(DBPediaTransform.crossArray(body["results"]["bindings"]));
       })
       .catch((err: any) => {
         return Bluebird.reject(err);
@@ -155,7 +179,7 @@ export namespace DBPedia {
      * Returns all available information about the anime 'name'.
      * @param name The anime's name.
      */
-    export function retrieve(name: string): Bluebird<Anime> {
+    export function retrieve(name: string): Bluebird<AnimeType> {
       return Bluebird.reject(new Error("This function is not implemented yet"));
     }
   }
@@ -169,7 +193,7 @@ export namespace DBPedia {
      * Returns all available information about the character 'name'.
      * @param name The character's name.
      */
-    export function retrieve(name: string): Bluebird<Character> {
+    export function retrieve(name: string): Bluebird<CharacterType> {
       return Bluebird.reject(new Error("This function is not implemented yet"));
     }
   }
@@ -184,7 +208,7 @@ export namespace DBPedia {
      * @param name The author's name.
      */
     // TODO: for the moment, only wraps the given name in an object.
-    export function retrieve(name: string): Bluebird<Author> {
+    export function retrieve(name: string): Bluebird<AuthorType> {
       return Bluebird.resolve({name: name});
     }
   }
@@ -235,144 +259,197 @@ export namespace DBPedia {
       encodeURI(BASE_RESOURCE_URL + "/" + stringToResourceName(name)) :
       encodeURI(name);
     }
+
+    /**
+     * Transforms a given string into an URL string.
+     * To be a valid DBPedia resource's URL, the resource's name
+     * must be a valid one.
+     * @param name The valid name of the resource.
+     */
+    export function resourceToResourceUrl(name: string): string {
+      return encodeURI(BASE_RESOURCE_URL + "/" + name);
+    }
+
+    /**
+     * Transforms a given URL into a resource string.
+     * To be a valid DBPedia resource's name, the resource's URL
+     * must be a valid one.
+     * @param url The valid resource's URL.
+     */
+    export function resourceUrlToResource(url: string): string {
+      return url.replace(new RegExp(BASE_RESOURCE_URL + "(.*)"), "$1");
+    }
   }
 }
 
 /**
- * Transform a sparql raw search result into a usable search result.
- * @param sparqlResult The raw sparql result.
+ * Private namespace for this file,
+ * containing all needed function to process results.
  */
-function sparqlToObjects(sparqlResult: any): DBPedia.Search.Result {
-  console.log("CROSS ARRAY:");
-  console.log(sparqlResult);
-  let res: DBPedia.Search.Result = {
-    manga: null,
-    anime: null,
-    author: null,
-    character: null
-  };
-  let tabRes: any[] = [[], [], [], []];
-  for(let result of sparqlResult) {
-    console.log(result);
-    if (result["x"]["value"] == 'manga') {
+namespace DBPediaTransform {
+
+  /**
+   * Transform a sparql raw search result into a usable search result.
+   * @param sparqlResult The raw sparql result.
+   */
+  // TODO: refactor this
+  export function sparqlToObjects(sparqlResult: any): DBPedia.Search.Result {
+    console.log("CROSS ARRAY:");
+    console.log(sparqlResult);
+    let res: DBPedia.Search.Result = {
+      manga: undefined,
+      anime: undefined,
+      author: undefined,
+      character: undefined
+    };
+    let tabRes: any[] = [[], [], [], []];
+    for(let result of sparqlResult) {
+      console.log(result);
+      if (result["x"]["value"] == 'manga') {
+        tabRes[0].push(result);
+      } else if (result["x"]["value"] == 'anime') {
+        tabRes[1].push(result);
+      } else if (result["x"]["value"] == 'author') {
+        tabRes[2].push(result);
+      } else if (result["x"]["value"] == 'character') {
+        tabRes[3].push(result);
+      }
       delete result["x"];
-      tabRes[0].push(result);
-    } else if (result["x"]["value"] == 'anime') {
-      tabRes[1].push(result);
-    } else if (result["x"]["value"] == 'author') {
-      tabRes[2].push(result);
-    } else if (result["x"]["value"] == 'character') {
-      tabRes[3].push(result);
     }
-  }
-  console.log(tabRes);
-  if(tabRes[0].length !== 0) {
-    res.manga = sparqlToManga(crossArray(tabRes[0]));
-  }
-  if(tabRes[1].length !== 0) {
-    res.anime = sparqlToAnime(crossArray(tabRes[1]));
-  }
-  if(tabRes[2].length !== 0) {
-    res.author = sparqlToAuthor(crossArray(tabRes[2]));
-  }
-  if(tabRes[3].length !== 0) {
-    res.character = sparqlToCharacter(crossArray(tabRes[3]));
-  }
-  return res;
-}
-
-/**
- * Transforms an object coming from a sparql request's response
- * into a manga.
- * It's fairly recommended to process the function crossArray
- * on the result BEFORE passing it to this function.
- * @param sparqlResult The result coming from a response to a sparql request.
- */
-function sparqlToManga(sparqlResult: any): Manga {
-  sparqlResult = sparqlResult.length !== undefined ? sparqlResult[0] : sparqlResult;
-  // TODO: find a way to type the following variable as Manga without throwing a typescript error
-  let manga: any = {};
-  for(let key in sparqlResult) {
-    if (!sparqlResult.hasOwnProperty(key)) continue;
-    if(sparqlResult[key].length === 1) {
-      manga[key] = (<any>sparqlResult[key][0]);
-    } else if(key === "publicationDate") {
-      manga[key] = _.min(sparqlResult[key]);
-    } else if(key === "author") {
-      manga[key] = sparqlResult[key][0];
-    } else if(key === "abstract") {
-      manga[key] = sparqlResult[key][0];
-    } else if(key === "volumes") {
-      manga[key] = _.max(sparqlResult[key]);
-    } else {
-      manga[key] = sparqlResult[key];
+    console.log(tabRes);
+    if(tabRes[0].length !== 0) {
+      res.manga = sparqlToManga(crossArray(tabRes[0]));
     }
-  }
-  return manga;
-}
-
-function sparqlToAnime(sparqlResult: any): Anime {
-  sparqlResult = sparqlResult.length !== undefined ? sparqlResult[0] : sparqlResult;
-  // TODO: find a way to type the following variable as Manga without throwing a typescript error
-  let anime: any = {};
-  for(let key in sparqlResult) {
-    if (!sparqlResult.hasOwnProperty(key)) continue;
-    if(sparqlResult[key].length === 1) {
-      anime[key] = (<any>sparqlResult[key][0]);
+    if(tabRes[1].length !== 0) {
+      res.anime = sparqlToAnime(crossArray(tabRes[1]));
     }
-  }
-  return anime;
-}
-
-function sparqlToAuthor(sparqlResult: any): Author {
-  sparqlResult = sparqlResult.length !== undefined ? sparqlResult[0] : sparqlResult;
-  // TODO: find a way to type the following variable as Manga without throwing a typescript error
-  let author: any = {};
-  for(let key in sparqlResult) {
-    if (!sparqlResult.hasOwnProperty(key)) continue;
-    if(sparqlResult[key].length === 1 && key === "title") {
-      author["name"] = (<any>sparqlResult[key][0]);
-    } else if(sparqlResult[key].length === 1) {
-      author[key] = (<any>sparqlResult[key][0]);
+    if(tabRes[2].length !== 0) {
+      res.author = sparqlToAuthor(crossArray(tabRes[2]));
     }
-  }
-  return author;
-}
-
-function sparqlToCharacter(sparqlResult: any): Author {
-  sparqlResult = sparqlResult.length !== undefined ? sparqlResult[0] : sparqlResult;
-  // TODO: find a way to type the following variable as Manga without throwing a typescript error
-  let author: any = {};
-  for(let key in sparqlResult) {
-    if (!sparqlResult.hasOwnProperty(key)) continue;
-    if(sparqlResult[key].length === 1 && key === "title") {
-      author["name"] = (<any>sparqlResult[key][0]);
-    } else if(sparqlResult[key].length === 1) {
-      author[key] = (<any>sparqlResult[key][0]);
+    if(tabRes[3].length !== 0) {
+      res.character = sparqlToCharacter(crossArray(tabRes[3]));
     }
+    return res;
   }
-  return author;
-}
 
-/**
- * Transform an array of same type objects in a object with arrays as fields,
- * deleting duplicated values in the process.
- * @param sparqlResult The raw result sent by dbpedia.
- */
-function crossArray(sparqlResult: any[]): any {
-  let res: any = {};
-  let first: boolean = true;
-  for(let object of sparqlResult) {
-    for(let key in object) {
-      if (!object.hasOwnProperty(key)) continue;
-      if(first) {
-        res[key] = [object[key]["value"]];
+  /**
+   * Transforms an object coming from a sparql request's response
+   * into a manga.
+   * @param sparqlResult The result coming from a response to a sparql request.
+   */
+  export function sparqlToManga(sparqlResult: RawResultArray): MangaType {
+    // TODO: find a way to type the following variable as Manga without throwing a typescript error
+    let manga: any = {};
+    for(let key in sparqlResult) {
+      if (!sparqlResult.hasOwnProperty(key)) continue;
+      if(sparqlResult[key].length === 1) {
+        manga[key] = (<any>sparqlResult[key][0]);
+      } else if(key === "publicationDate") {
+        manga[key] = _.min(sparqlResult[key]);
+      } else if(key === "author") {
+        manga[key] = sparqlResult[key][0];
+      } else if(key === "abstract") {
+        manga[key] = sparqlResult[key][0];
+      } else if(key === "volumes") {
+        manga[key] = _.max(sparqlResult[key]);
       } else {
-        if(res[key].indexOf(object[key]["value"]) !== -1) continue;
-        res[key].push(object[key]["value"]);
+        manga[key] = sparqlResult[key];
       }
     }
-    first = false;
+    return manga;
   }
-  return res;
+
+  /**
+   * Transforms an object coming from a sparql request's response
+   * into a anime.
+   * @param sparqlResult The result coming from a response to a sparql request.
+   */
+  export function sparqlToAnime(sparqlResult: RawResultArray): AnimeType {
+    // TODO: find a way to type the following variable as Anime without throwing a typescript error
+    let anime: any = {};
+    for(let key in sparqlResult) {
+      if (!sparqlResult.hasOwnProperty(key)) continue;
+      if(sparqlResult[key].length === 1) {
+        anime[key] = (<any>sparqlResult[key][0]);
+      }
+    }
+    return anime;
+  }
+
+  /**
+   * Transforms an object coming from a sparql request's response
+   * into an author.
+   * @param sparqlResult The result coming from a response to a sparql request.
+   */
+  export function sparqlToAuthor(sparqlResult: RawResultArray): AuthorType {
+    // TODO: find a way to type the following variable as Author without throwing a typescript error
+    let author: any = {};
+    for(let key in sparqlResult) {
+      if (!sparqlResult.hasOwnProperty(key)) continue;
+      if(sparqlResult[key].length === 1 && key === "title") {
+        author["name"] = (<any>sparqlResult[key][0]);
+      } else if(sparqlResult[key].length === 1) {
+        author[key] = (<any>sparqlResult[key][0]);
+      }
+    }
+    return author;
+  }
+
+  /**
+   * Transforms an object coming from a sparql request's response
+   * into a character.
+   * @param sparqlResult The result coming from a response to a sparql request.
+   */
+  export function sparqlToCharacter(sparqlResult: RawResultArray): AuthorType {
+    // TODO: find a way to type the following variable as Character without throwing a typescript error
+    let author: any = {};
+    for(let key in sparqlResult) {
+      if (!sparqlResult.hasOwnProperty(key)) continue;
+      if(sparqlResult[key].length === 1 && key === "title") {
+        author["name"] = (<any>sparqlResult[key][0]);
+      } else if(sparqlResult[key].length === 1) {
+        author[key] = (<any>sparqlResult[key][0]);
+      }
+    }
+    return author;
+  }
+
+  /**
+   * Transform an array of same type objects in a object with arrays as fields,
+   * deleting duplicated values in the process.
+   * @param sparqlResult The raw result sent by dbpedia.
+   */
+  export function crossArray(sparqlResult: any[]): RawResultArray {
+    // TODO: find a way to type the following variable without typescript throwing an error
+    let res: any = {};
+    let first: boolean = true;
+    for(let object of sparqlResult) {
+      for(let key in object) {
+        if (!object.hasOwnProperty(key)) continue;
+        if(first) {
+          res[key] = [object[key]["value"]];
+        } else {
+          if(res[key].indexOf(object[key]["value"]) !== -1) continue;
+          res[key].push(object[key]["value"]);
+        }
+      }
+      first = false;
+    }
+    return <RawResultArray>res;
+  }
+
+  /**
+   * A sparql raw result array transformed into a single object.
+   */
+  export interface RawResultArray {
+    x: string[];               // "manga", "anime" and so on
+    title: string[];           // Anime/Manga's title or people's name
+    author: string[];          // Author of the manga/anime
+    volumes: number[];         // Number of volumes or episodes
+    publicationDate: string[]; // First volume/episode publication date or people's birthday
+    illustrator: string[];     // Illustrator of the manga/anime
+    publisher: string[];       // Publisher's name
+    abstract: string[];        // Manga/Anime's abstract or snippet about people
+    [key: string]: any[];      // To use syntax obj["key"]
+  }
 }
