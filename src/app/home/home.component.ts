91bb1bee472484e5e2fda05456035bec7c3d8eca
Nicolas <nicolas.six@insa-lyon.fr>
2017-01-03 12:34:35 +0100

    Removed useless view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,6 @@ import {Component} from "@angular/core";
 @Component({
   selector: "home",
   moduleId: "home/home.component",
-  template: "Accueil, News, toussa"
+  template: ""
 })
 export class HomeComponent {}
