35652f14e72d6fb860f5f32bde9a5a97c966580b
Charles Samborski <demurgos@demurgos.net>
2017-01-03 03:58:33 +0100

    Fix undefined error un app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,6 @@ import {Manga} from "../lib/interfaces/manga.interface";
 import {appEnvironment, Environment} from "./app.tokens";
 import {SearchBarComponent} from "./search/search-bar/search-bar.component";
 
-const MANGATEST: Manga = {
-  title: "Death Note",
-  author: {name: "Tsugumi Oba"},
-  illustrator: ["Takeshi Obata"],
-  volumes: 12,
-  genres: ["Thriller", "Drama"],
-  abstract: "Kira kill the world with a book from hell",
-  coverUrl: "http://mcd.iosphe.re/n/41/1/front/a/"
-};
-
 @Component({
   selector: "mmg-app",
   moduleId: "app.component",
@@ -23,18 +13,9 @@ const MANGATEST: Manga = {
 })
 export class AppComponent {
   private environment: Environment;
-  manga: Manga;
   search: SearchBarComponent;
 
   constructor(@Inject(appEnvironment) environment: Environment) {
     this.environment = environment;
-    console.log("Environment:");
-    console.log(this.environment);
   }
-
-  ngOnInit(): void {
-    // this.manga = MANGATEST;
-    console.log(this.manga.title);
-  }
-
 }
